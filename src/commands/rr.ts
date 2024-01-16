e49918339aabb9852290c8bd5954ac5a08002576
InfiniteImperm <84783592+InfiniteImperm@users.noreply.github.com>
2024-01-16 20:32:33 +0100

    Update rr.ts
    changed commands to something other than bp/bestping

--- src/commands/rr.ts
+++ src/commands/rr.ts
@@ -2,7 +2,7 @@ import { ForestBotApiClient } from "forestbot-api";
 import type Bot from "../structure/mineflayer/Bot.js";
 
 export default {
-  commands: ["bp", "bestping"],
+  commands: ["rr", "radicalraiders"],
   minArgs: 0,
   maxArgs: 1,
   execute: async (user, args, bot, api: ForestBotApiClient) => {
